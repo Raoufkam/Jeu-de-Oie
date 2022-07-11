b5edbbd6ab5296f0d1f7d0ba3d3e620fb81f55bc
Kamiche Mohamed Raouf <93711354+Raoufkam@users.noreply.github.com>
2022-07-11 20:19:05 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,10 +1,16 @@
 package jeuOie;
 
+import java.io.*;
+
 public class Test {
-	  public static void main(String argv[]){
-	    JeuOie jeuOie = new JeuOie(2); 
+	  	public static void main(String argv[]) throws FileNotFoundException {
+		
+		FileOutputStream JeuOie = new FileOutputStream("Resultat_Partie.txt") ;
+		System.setOut(new PrintStream(JeuOie));
+	    JeuOie jeuOie = new JeuOie(2);
 	    jeuOie.ajouterOie("verte");
 	    jeuOie.ajouterOie("jaune");
 	    jeuOie.jouer();
 	  }
+
 }
